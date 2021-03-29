fc6330ace543adb301859d42d5a31a7e477d5233
morisho04 <sho04010909@gmail.com>
2021-03-29 20:41:46 +0900

    mori

--- naomori.js
+++ naomori.js
@@ -1,9 +1,15 @@
 function hamburger() {
+    
     document.getElementById('line1').classList.toggle('line_1');
     document.getElementById('line2').classList.toggle('line_2');
     document.getElementById('line3').classList.toggle('line_3');
     document.getElementById('nav').classList.toggle('in');
+    
   }
+function foo() {
+    document.getElementById('line1').style.zIndex=103;
+}
   document.getElementById('mori-b').addEventListener('click' , function () {
     hamburger();
+    foo();
   } );
